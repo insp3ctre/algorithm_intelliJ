fb68b6b8ac33c9455a44c5db7786ac458ef46ce1
insp3ctre <67237263+insp3ctre@users.noreply.github.com>
2021-09-05 11:50:22 -0700

    added log

---
+++ src/homework/Logarithms.java
@@ -0,0 +1,13 @@
+package homework;
+
+public class Logarithms {
+
+   public static int log(int a, int b) {
+       int count = 0;
+       while (b > 1) {
+           b = b / a;
+           count++;
+       }
+       return count;
+   }
+}
