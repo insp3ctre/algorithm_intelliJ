07850b6fe8cacc31b2a777953142a17111d511fa
insp3ctre <67237263+insp3ctre@users.noreply.github.com>
2021-09-08 12:44:28 -0700

    fixed log

--- src/homework/Logarithms.java
+++ src/homework/Logarithms.java
@@ -4,10 +4,10 @@ public class Logarithms {
 
    public static int log(int a, int b) {
        int count = 0;
-       while (b > 1) {
+       while (b >= 1) {
            b = b / a;
            count++;
        }
-       return count;
+       return count-1;
    }
 }
