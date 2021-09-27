5de0f3372c89399daa2a775315b8219baa23fc39
insp3ctre <67237263+insp3ctre@users.noreply.github.com>
2021-09-26 18:57:49 -0700

    updated bubble to a more sophisticated method

--- src/homework/Bubble.java
+++ src/homework/Bubble.java
@@ -2,19 +2,39 @@ package homework;
 
 public class Bubble {
     /**
-     * Sorts an array of doubles using the bubble sort method
+     * Sorts an array of comparables using the bubble sort method
      * @param b the array you wish to bubble sort
      */
-    public static void sort(Double[] b) {
-        double temp;
+    public static void sort(Comparable[] b) {
+        Comparable temp;
         for (int c = 0; c < b.length; c++) {
             for (int n = 0; n < b.length-1; n++) {
-                if (b[n] > b[n+1]) {
-                    temp = b[n];
-                    b[n] = b[n+1];
-                    b[n+1] = temp;
+                if (less(b[n+1], b[n])) {
+                    exch(b, n, n+1);
                 }
             }
         }
+    }
+
+    /**
+     * Compare two comparables to see which is smaller
+     * @param v a comparable
+     * @param w a comparable
+     * @return True if the comparable is smaller and False if it is not
+     */
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    /**
+     * Swap the comparables
+     * @param a a temporary object
+     * @param i a comparable
+     * @param j a comparable
+     */
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
     }
 }
