7288ebeef7e486f1cdd1bb319b332db018947531
insp3ctre <67237263+insp3ctre@users.noreply.github.com>
2021-10-17 20:54:26 -0700

    fixed bloomfilter

--- src/bloom/BloomFilter.java
+++ src/bloom/BloomFilter.java
@@ -1,19 +1,14 @@
+/** Charlie Compton, Berto Gonzalez, Ross Miyabuchi, and Adam Schwarz **/
+
 package bloom;
 
 public class BloomFilter<S> {
-
-   // private long[] bitTable = new long[1024];
-    private static final int MASK = (1<<16)-1;
 
     private long[] bitTable;
     public BloomFilter() {
         bitTable = new long[1024];
     }
-
-    public long hashCode(S object) {
-        int hashCode = object.hashCode();
-        return hashCode;
-    }
+    private static final int MASK = (1<<16)-1;
 
     public void addBit(int key) {
         bitTable[key/64] |= 1L << (key % 64);
@@ -36,9 +31,11 @@ public class BloomFilter<S> {
 
     public long trueBits() {
         int bitCount = 0;
-        for (int i = 0; i < 1024; i++) {
-            if (bitTable[i] != 0) {
+        for (long counter : bitTable) {
+            long counter2 = counter;
+            while (counter2 != 0) {
                 bitCount++;
+                counter2 = (counter2 - 1) & counter2;
             }
         }
         return bitCount;
